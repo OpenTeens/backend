fc9fd2adb550b08c3a4098714290e0b6ac54ead5
Thomas Wang <55052188+cxzlw@users.noreply.github.com>
2024-07-15 20:21:25 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from gateway.apigateway import APIGateway
+
+def run(self):
+    def make_gateway(s):
+        def gateway(path):
+            return self.process(s, path, flask.request)
+
+        return gateway
+
+    for _, s in self.services.items():
+        self.app.route(
+            f"/{s.prefix}/<path:path>",
+            methods=["GET", "POST", "PUT", "DELETE"],
+            endpoint=f"gw_{s.prefix}",
+        )(make_gateway(s))
+
+
+gateway = APIGateway()
+run(gateway)
+app = gateway.app
