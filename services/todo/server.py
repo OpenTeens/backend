e2ce0be054f4fbd80a9e62e0c04f9b62fba17324
berniehuang2008 <berniehuang2008@163.com>
2024-07-15 15:05:25 +0800

    [fix] fix the volunabiliy that user can construct a melicious tablename to perform a SQL injection
    Co-Authored-By: cxzlw

--- services/todo/server.py
+++ services/todo/server.py
@@ -2,13 +2,21 @@ import todo_db
 
 import flask
 from flask_cors import CORS
+from hashlib import sha256, sha512
+
 
 app = flask.Flask(__name__)
 CORS(app, origins="*")
 
 
+def _hash(s):
+    return sha256(s.encode("utf-8")).hexdigest() + sha512(s.encode("utf-8")).hexdigest()
+
+
 @app.route("/create/<listID>", methods=["GET"])
 def createList(listID):
+    listID = _hash(listID)
+
     if todo_db.addTable(listID):
         return {"code": 0, "msg": "success", "listID": listID}
     else:
@@ -17,6 +25,8 @@ def createList(listID):
 
 @app.route("/<listID>/addTask", methods=["POST"])
 def addTask(listID: str):
+    listID = _hash(listID)
+
     data = flask.request.json
     # default
     default = {"title": "", "content": "", "ddl": "", "manager": "", "reviewer": ""}
@@ -38,6 +48,8 @@ def addTask(listID: str):
 
 @app.route("/<listID>/updateTask", methods=["POST"])
 def updateTask(listID: str):
+    listID = _hash(listID)
+
     data = flask.request.json
     id = data["id"]
     todo_db.updateTask(listID, id, **data["update"])
@@ -46,19 +58,31 @@ def updateTask(listID: str):
 
 @app.route("/<listID>/getTasks", methods=["GET"])
 def getTasks(listID: str):
+    listID = _hash(listID)
+
     if listID not in todo_db.db.tables:
         createList(listID)
-    
+
     tasks = todo_db.getTasks(listID)
     return {"code": 0, "msg": "success", "tasks": list(tasks)}
 
 
 @app.route("/<listID>/getTask", methods=["GET"])
 def getTask(listID: str):
+    listID = _hash(listID)
+
     id = flask.request.args.get("id")
     task = todo_db.getTask(listID, id)
     return {"code": 0, "msg": "success", "task": list(task)}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True, ssl_context=('/home/bernie/cert/certificate.crt', '/home/bernie/cert/private.key')))
+    app.run(
+        host="0.0.0.0",
+        port=5001,
+        debug=True,
+        ssl_context=(
+            "/home/bernie/cert/certificate.crt",
+            "/home/bernie/cert/private.key",
+        ),
+    )
